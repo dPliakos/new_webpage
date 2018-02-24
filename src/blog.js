ba7e94ad2a09bf78386e7e4252e023b4fc2ab012
dpliakos <dimitrispl96@gmail.com>
2018-02-24 19:28:28 +0200

    remove unused import to fix depedences

--- src/blog.js
+++ src/blog.js
@@ -3,7 +3,6 @@ import BlogPost from './blogpost.js';
 import ERROR from './not_found.js';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import Sidebar from 'react-sidebar';
 
 export default class Blog extends React.Component {
   render() {
