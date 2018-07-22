8f01707d68b5170d653922bd33186a89c272f1ad
dpliakos <dimitrispl96@gmail.com>
2018-07-23 00:19:18 +0300

    theme event teaser

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import ExpandibleCard from './components/event-teaser';
 
 class App extends React.Component {
   render() {
+
+    const event = {
+      title: 'title',
+      date: new Date(),
+      subtitle: 'subtitle',
+      description: 'Lorem ipsum dolor sit amet, meis adipiscing at per, at mea suscipit oportere. Nostrum salutandi an his, eu vel invidunt accusamus, ferri facilisi consulatu per te. No tale nominavi usu. Ut veritus referrentur vim, hinc volumus cu cum, enim incorrupte ius ut. Sit ex appareat disputationi, an quando accumsan insolens ius.',
+      location: 'Thessaloniki',
+      organizer: 'IEEE student branch',
+      link: 'http://google.com'
+    }
+
     return (
       <div>
-        Hello World
+        <ExpandibleCard event={event} />
       </div>
     );
   }
--- src/app/components/event-teaser.js
+++ src/app/components/event-teaser.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import IconLabel from './icon-label';
+import {Collapse} from 'react-bootstrap';
 
 class ExpandibleCard extends React.Component {
 
@@ -10,26 +11,28 @@ class ExpandibleCard extends React.Component {
     }
   }
 
+  toggle = ()=>{this.setState({open: !this.state.open})}
+
   render() {
     const event = this.props.event;
 
     const title = event.title || '';
     const subtitle = event.subtitle || '';
-    const day = event.date.getDate();
-    const month = event.date.getMonth() + 1;
-    const hours = event.date.getHours();
-    const mins = event.date.getMinutes();
+    const day = ( '00' + event.date.getDate()).substr(-2);
+    const month = ( '00' + (event.date.getMonth() + 1)).substr(-2);
+    const hours = ('00' + event.date.getHours()).substr(-2);
+    const mins = ('00' + event.date.getMinutes()).substr(-2);
     const description = event.description;
 
     const date_upper = `${day}/${month}`;
     const date_lower = `${hours}:${mins}`;
 
     const location_label = event.location || 'no location found';
-    const location = <IconLabel label={location_label} icon='location' />
+    const location = <IconLabel label={location_label} icon='location-arrow' />
 
     let organizer = <div></div>;
     if (event.organizer) {
-      organizer = <IconLabel label={event.organizer} icon='organizer' />
+      organizer = <IconLabel label={event.organizer} icon='users' />
     }
 
     let link = <div></div>;
@@ -41,7 +44,7 @@ class ExpandibleCard extends React.Component {
 
     return (
       <div className={`event-teaser ${extraClasses}`}>
-        <div className="event-teaser__header">
+        <div className="event-teaser__header" onClick={this.toggle}>
           <div className="event-teaser__date">
             <div className="event-teaser__date--date">
               {date_upper}
@@ -58,18 +61,23 @@ class ExpandibleCard extends React.Component {
               {subtitle}
             </div>
           </div>
-          <div className="event-teaser__button"></div>
-        </div>
-        <div className="event-teaser__body">
-          <div className="event-teaser__basic-info">
-            {location}
-            {organizer}
-            {link}
-          </div>
-          <div className="event-teaser__description">
-            {description}
+          <div className="event-teaser__button">
+            <span class="fas fa-angle-double-up"></span>
           </div>
         </div>
+        <Collapse in={this.state.open}>
+        {/* Collapse works only with a single div as it's child */}
+          <div className="event-teaser__body">
+              <div className="event-teaser__basic-info">
+                {location}
+                {organizer}
+                {link}
+              </div>
+              <div className="event-teaser__description">
+                {description}
+              </div>
+          </div>
+        </Collapse>
       </div>
     );
   }
